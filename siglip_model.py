fc888735a31dd99c0873ff51355218f2099aa5d5
Sudhin05 <sudhindrapai11@gmail.com>
2024-11-20 19:12:22 +0530

    Add SiglipMLP

--- siglip_model.py
+++ siglip_model.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 class SiglipVisionConfig:
   def __init__(
@@ -80,25 +81,39 @@ class SiglipVisionEmbeddings(nn.Module):
 
     return embeddings
 
-# class SiglipEncoderLayer(nn.Module):
-#   def __init__(self,config: SiglipVisionConfig):
-#     super(SiglipEncoderLayer, self).__init__()
-#     self.embed_dim = config.hidden_size
-#     self.eps = config.layer_norm_eps
-#     self.self_attn = SiglipAttention(config)
-#     self.layer_norm1 =nn.LayerNorm(self.embed_dim,eps = self.eps)
-#     self.mlp = SiglipMLP(config)
-#     self.layer_norm2 =nn.LayerNorm(self.embed_dim,eps = self.eps)
+class SiglipMLP(nn.Module):
+  def __init__(self,config):
+    super().__init__()
+    self.config = config
+    self.fc1 = nn.Linear(config.hidden_size,config.intermediate_size)
+    self.fc2 = nn.Linear(config.intermediate_size,config.hidden_size)
 
-#   def forward(self,hidden_states: torch.Tensor)->torch.Tensor:
-#     residual = hidden_states
-#     hidden_states = self.layer_norm1(hidden_states)
-#     hidden_states,_ = self.self_attn(hidden_states)
-#     residual = hidden_states + residual
-#     hidden_states = self.layer_norm2(residual)
-#     hidden_states = self.mlp(hidden_states)
-#     hidden_states = hidden_states + residual
-#     return hidden_states
+  def forward(self,hidden_states: torch.Tensor) -> torch.Tensor:
+    hidden_states = self.fc1(hidden_states)
+    hidden_states = F.gelu(hidden_states,approximate="tanh")
+    hidden_states = self.fc2(hidden_states)
+
+    return hidden_states
+
+class SiglipEncoderLayer(nn.Module):
+  def __init__(self,config: SiglipVisionConfig):
+    super(SiglipEncoderLayer, self).__init__()
+    self.embed_dim = config.hidden_size
+    self.eps = config.layer_norm_eps
+    self.self_attn = SiglipAttention(config)
+    self.layer_norm1 =nn.LayerNorm(self.embed_dim,eps = self.eps)
+    self.mlp = SiglipMLP(config)
+    self.layer_norm2 =nn.LayerNorm(self.embed_dim,eps = self.eps)
+
+  def forward(self,hidden_states: torch.Tensor)->torch.Tensor:
+    residual = hidden_states
+    hidden_states = self.layer_norm1(hidden_states)
+    hidden_states,_ = self.self_attn(hidden_states)
+    residual = hidden_states + residual
+    hidden_states = self.layer_norm2(residual)
+    hidden_states = self.mlp(hidden_states)
+    hidden_states = hidden_states + residual
+    return hidden_states
     
 
 class SiglipVisionTransformer(nn.Module):
